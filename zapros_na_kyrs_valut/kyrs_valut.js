735a9f4c6b9769ed667cf11c44ba644e5d83e4c9
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-11-28 00:27:02 +0200

    запрос на курс валют - done

--- zapros_na_kyrs_valut/kyrs_valut.js
+++ zapros_na_kyrs_valut/kyrs_valut.js
@@ -1,28 +1,49 @@
-angular.module("myApp",[]).controller('ignitionMsgCtrl', ['$scope', function ($scope) {
+angular.module("myApp", []).controller('valutCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.allChatMessages = [];
     $scope.newMessageText = null;
     $scope.newMessageFrom = null;
 
-    $scope.existingMessageText;
-    $scope.existingMessageFrom;
-
-    $scope.onSendNewMessageClicked = function () {
-        //alert('hi');
+    $scope.onGetValuteClicked = function () {
         getChatMessages();
     };
 
     function getChatMessages() {
-        var promise = $http.get('https://api.fixer.io/latest?symbols=33,44');
-        //alert('hi');
 
+        document;
+        window;
+
+        var promise = $http.get('https://api.fixer.io/latest');  //https://api.fixer.io/latest
         promise.then(function (response) {
 
-            alert(response.data.this.base);
+            var receivedData = response.data;
+            var receivedRates = receivedData.rates;
 
-
-
+            var eurToUsd = receivedRates.USD;
+            debugger;
         });
+
+        /*
+        $.getJSON(
+            // NB: using Open Exchange Rates here, but you can use any source!
+            'https://openexchangerates.org/api/latest.json?app_id=[YOUR APP ID]',
+            function(data) {
+                // Check money.js has finished loading:
+                if ( typeof fx !== "undefined" && fx.rates ) {
+                    fx.rates = data.rates;
+                    fx.base = data.base;
+                } else {
+                    // If not, apply to fxSetup global:
+                    var fxSetup = {
+                        rates : data.rates,
+                        base : data.base
+                    }
+                }
+            }
+        );
+        */
+
+
     }
 
 
