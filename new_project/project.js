2ca0a3c16437c0bbf2866776000ea161bf389051
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-11-29 03:57:07 +0200

    выпадающий список

--- new_project/project.js
+++ new_project/project.js
@@ -1,27 +1,45 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('mainCtrl', function($scope){
+myApp.controller('mainCtrl',['$scope', '$http', function ($scope,$http) {
+    $scope.info=[];
+    $scope.info_1=[];
 
 
-    $scope.myOnSubmitHandler = function(){alert('ok')};//main function
+    $scope.getListOfCurrencies = function() {
 
 
-});
+        var promise = $http.get('https://api.fixer.io/2000-01-03');
+        promise.then(function (response) {
 
-myApp.directive('Directive', function(){
-debudder;
+            $scope.info=response.data.rates;
+            $scope.info_1=Object.keys($scope.info);
+            //console.log(typeof($scope.info_1[0]));
+
+            debugger;
+
+        });
+
+
+    };//main function
+
+
+}]);
+
+
+
+myApp.directive('mySelect2Directive', function () {
+
     return {
-        link: function(scope, element, attrs) {
+        link: function (scope, element, attrs) {
+            scope.getListOfCurrencies();
+            //debugger;
+            $(element).select2();
 
-
-
-            $(".my-select2-directive").select2()
-
+            $(element).select2({
+                data: scope.info_1
+            });
 
         }
-
-            //$('.my-select2-directive').select2()
-        }
-
+    }
 
 });
