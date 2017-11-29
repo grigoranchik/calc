5ed8aa561f4e040d01e24adbbacd73604e40e864
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-11-29 15:55:22 +0200

    выпадающий список

--- new_project/project.js
+++ new_project/project.js
@@ -1,45 +1,36 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainCtrl',['$scope', '$http', function ($scope,$http) {
-    $scope.info=[];
-    $scope.info_1=[];
+myApp.controller('mainCtrl', ['$scope', '$http', function ($scope, $http) {
 
-
-    $scope.getListOfCurrencies = function() {
-
-
-        var promise = $http.get('https://api.fixer.io/2000-01-03');
-        promise.then(function (response) {
-
-            $scope.info=response.data.rates;
-            $scope.info_1=Object.keys($scope.info);
-            //console.log(typeof($scope.info_1[0]));
-
-            debugger;
-
-        });
-
-
-    };//main function
 
 
 }]);
 
 
-
-
-myApp.directive('mySelect2Directive', function () {
+myApp.directive('mySelect2Directive', function ($http) {
 
     return {
         link: function (scope, element, attrs) {
+
+            scope.info = [];
+            scope.info_1 = [];
+
+            scope.getListOfCurrencies = function () {
+
+                var promise = $http.get('https://api.fixer.io/2000-01-03');
+                promise.then(function (response) {
+
+                    scope.info = response.data.rates;
+                    scope.info_1 = Object.keys(scope.info);
+
+                    $(element).select2({
+                        data: scope.info_1
+                    });
+
+                });
+            };
+
             scope.getListOfCurrencies();
-            //debugger;
-            $(element).select2();
-
-            $(element).select2({
-                data: scope.info_1
-            });
-
         }
     }
 
