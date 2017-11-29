f35348e3d4b3e13f48565b8dfe048e872a11756e
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-11-29 16:10:03 +0200

    выпадающий список promise

--- y_promise/my_promise.js
+++ y_promise/my_promise.js
@@ -13,18 +13,24 @@ myApp.directive('myPromiseDirective', function ($http, $q) {
         link: function (scope, element, attrs) {
 
             scope.getListOfCurrencies = function () {
+                var deferred = $q.defer();
+
                 var promise = $http.get('https://api.fixer.io/2000-01-03');
                 promise.then(function (response) {
 
                     var info = response.data.rates;
-                    var info_1 = Object.keys(scope.info);
+                    var info_1 = Object.keys(info);
+                    deferred.resolve(info_1);
                 });
+
+                return deferred.promise;
             };
 
             var getListPromise = scope.getListOfCurrencies();
             getListPromise.then(function (listOfCurr) {
+
                 $(element).select2({
-                    data: scope.info_1
+                    data: listOfCurr
                 });
             });
         }
