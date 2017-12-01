1d5963bc4c30592c6258784f107fee8d12b1e44e
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-12-01 20:55:50 +0200

    работа с файлами отправка/получка файлов(1 вариант)

--- Work_with_files(Jquery_obrabotchik_sobitij_na_pole_vibora_faila)/work_with_files.js
+++ Work_with_files(Jquery_obrabotchik_sobitij_na_pole_vibora_faila)/work_with_files.js
@@ -1,7 +1,6 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('mainCtrl', function(){
-
+myApp.controller('mainCtrl',['$http', function($http){
 
     var files; // переменная. будет содержать данные файлов
 
@@ -11,67 +10,36 @@ myApp.controller('mainCtrl', function(){
         console.log(files);
     });
 
-
-    // обработка и отправка AJAX запроса при клике на кнопку upload_files
     $('.upload_files').on( 'click', function( event ){
-
-        event.stopPropagation(); // остановка всех текущих JS событий
-        event.preventDefault();  // остановка дефолтного события для текущего элемента - клик для <a> тега
-
-        // ничего не делаем если files пустой
-        if( typeof files == 'undefined' ) return;
 
         // создадим объект данных формы
         var data = new FormData();
+        data.append('file', files[0]);
+        console.log(data);
 
-        // заполняем объект данных файлами в подходящем для отправки формате
-        $.each( files, function( key, value ){
-            data.append( key, value );
+        var promise = $http.post('http://176.36.229.152/ignition/rest/files/upload', data , {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
         });
+        promise.then(function (){
 
-        // добавим переменную для идентификации запроса
-        data.append( 'my_file_upload', 1 );
-
-        // AJAX запрос
-        $.ajax({
-            url         : 'http://176.36.229.152/ignition/rest/files/upload',
-            type        : 'POST', // важно!
-            data        : data,
-            cache       : false,
-            dataType    : 'json',
-            // отключаем обработку передаваемых данных, пусть передаются как есть
-            processData : false,
-            // отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-            contentType : false,
-            // функция успешного ответа сервера
-            success     : function( respond, status, jqXHR ){
-
-                // ОК - файлы загружены
-                if( typeof respond.error === 'undefined' ){
-                    // выведем пути загруженных файлов в блок '.ajax-reply'
-                    var files_path = respond.files;
-                    var html = '';
-                    $.each( files_path, function( key, val ){
-                        html += val +'<br>';
-                    } )
-
-                    $('.ajax-reply').html( html );
-                }
-                // ошибка
-                else {
-                    console.log('ОШИБКА: ' + respond.error );
-                }
-            },
-            // функция ошибки ответа сервера
-            error: function( jqXHR, status, errorThrown ){
-                console.log( 'ОШИБКА AJAX запроса: ' + status, jqXHR );
-            }
+        }).catch(function(error) {
+            console.log(error.status);
 
         });
-
     });
 
 
 
+    $('.save_files').on( 'click', function( event ) {
 
-});
+            var promise = $http.get('http://176.36.229.152:80/ignition/rest/files/download/');
+            var got_file;
+            promise.then(function (response) {
+                got_file=response.data;
+                console.log('saved_file:', got_file);
+            });
+
+    });
+
+}]);
