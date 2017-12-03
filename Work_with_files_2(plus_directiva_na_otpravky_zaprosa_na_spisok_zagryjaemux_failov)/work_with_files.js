548e06aa6c39ad4aa4f417bc1321a6ebb18ab828
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-12-04 00:41:38 +0200

    работа с файлами отправка/получка файлов(2 вариант)

--- Work_with_files_2(plus_directiva_na_otpravky_zaprosa_na_spisok_zagryjaemux_failov)/work_with_files.js
+++ Work_with_files_2(plus_directiva_na_otpravky_zaprosa_na_spisok_zagryjaemux_failov)/work_with_files.js
@@ -25,7 +25,7 @@ myApp.controller('mainCtrl',['$http', function($http){
     });
 }]);
 
-//конвертирует дату с милисекунд и проверяет сегоднешняя она, вчерашняя или раньше..
+//конвертирует дату с милисекунд на заданный формат, проверяет сегоднешняя она, вчерашняя или раньше..
 myApp.filter('fileDateFilter', function() {
         return function(input) {
             var dt=new Date();
@@ -37,17 +37,55 @@ myApp.filter('fileDateFilter', function() {
             //console.log(day+'.'+month+'.'+year);
             if(new Date(input).toString('dd.MM.yyyy')==day+'.'+month+'.'+year)
             {
-                return 'today';
+                return ('today'+new Date(input).toString(' HH:mm'));
             }
                 else
                     if(new Date(input).toString('dd.MM.yyyy')==parseInt(day.substring(1))+1+'.'+month+'.'+year)
                     {
-                        return 'tomorrow';
+                        return ('tomorrow'+new Date(input).toString(' HH:mm'));
                     }
                     else
                         return new Date(input).toString('dd.MM.yyyy HH:mm');
         };
     });
+
+//отображает расширение  ////////// <img src="icons/ico_html.png">
+myApp.filter('fileExtensionFilter', function() {
+    return function(input) {
+        if  (input.substring(input.indexOf('.'))==".txt")
+        {
+            return "icons/ico_txt.png";
+        }
+            else
+                if(input.substring(input.indexOf('.'))==".jpg")
+                {
+                    return "icons/ico_jpg.png"
+                }
+                    else
+                        if(input.substring(input.indexOf('.'))==".jpeg")
+                        {
+                            return "icons/ico_jpeg.png"
+                        }
+                            else
+                                if(input.substring(input.indexOf('.'))==".html")
+                                {
+                                    return "icons/ico_html.png"
+                                }
+                                    else
+                                        if(input.substring(input.indexOf('.'))==".js")
+                                        {
+                                            return "icons/ico_js.png"
+                                        }
+                                            else
+                                                if(input.substring(input.indexOf('.'))==".mp3")
+                                                {
+                                                    return "icons/ico_mp3.png"
+                                                }
+                                                else
+                                                    return;
+
+    };
+});
 
 
 //директива на отправку запроса на список файлов, вывода их ввиде списка ссылок
@@ -55,30 +93,29 @@ myApp.directive('mySelect2Directive', function ($http) {
     return {
         link: function (scope, element, attrs) {
             scope.info;
+            debugger;
             scope.info_1 = [];
             scope.getList = function () {
                 var promise = $http.get('http://176.36.229.152:80/ignition/rest/files/list');
                 promise.then(function (response) {
                     scope.info = response.data.availableFilesList;
-                    //for(i=0;i<scope.info.length;i++){
-                      //  scope.info[i].availFileDateAddedMilliseconds=new Date(scope.info[i].availFileDateAddedMilliseconds).toString('dd.MM.yyyy HH:mm');
-                    //};
+
                 }).catch(function(error) {
                     console.log(error.status);
                 });
             };
+            scope.getList();
 
             //отправить запрос на удаление файла
             scope.setDelete = function(elem){
-                var promise = $http.post('http://176.36.229.152:80/ignition/rest/files/delete/'+elem,{},{});
-                promise.then(function (){
-
+                var promise = $http.post('http://176.36.229.152:80/ignition/rest/files/delete/'+elem.availFileName,{},{});
+                promise.then(function (response){
+                   // debugger;
                 }).catch(function(error) {
+                    //debugger;
                     console.log(error.status);
                 });
             };
-
-            scope.getList();
         }
     }
 });
