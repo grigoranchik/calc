908b585571b407f3d9d25dd280f5a54deed9f617
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-12-05 22:04:58 +0200

    работа с файлами отправка/получка файлов(2 вариант)

--- Work_with_files_2(plus_directiva_na_otpravky_zaprosa_na_spisok_zagryjaemux_failov)/myDeleteFileDirective.js
+++ Work_with_files_2(plus_directiva_na_otpravky_zaprosa_na_spisok_zagryjaemux_failov)/myDeleteFileDirective.js
@@ -6,33 +6,30 @@ myApp.directive('myDeleteFileDirective', ['$http', 'serviceForGetListOfFiles', f
     return {
         link: function (scope, element, attrs) {
 
+
+
             //отправить запрос на удаление файла
             scope.setDelete = function (elem) {
 
                 if (confirm("Вы действительно хотите удалить файл?")) {
 
-                    $('.hide').css({
-                        'display': 'block'
-                    });
+                    scope.$emit('myFilesListChangingEvent', true);
+
                     //debugger;
                     var promise = $http.post('http://176.36.229.152:80/ignition/rest/files/delete/' + elem.availFileName, {});
                     promise.then(function (response) {
                         serviceForGetListOfFiles.getFilesListFromServer();
-                        $('.hide').css({
-                            'display': 'none'
-                        });
+
+                        scope.$emit('myFilesListChangingEvent', false);
 
                     }).catch(function (error) {
                         serviceForGetListOfFiles.getFilesListFromServer();
                         console.log(error.status);
-                        $('.hide').css({
-                            'display': 'none'
-                        });
+                        scope.$emit('myFilesListChangingEvent', false);
                     });
 
                 } else {
                     alert("нехуй нажимать не подумав!")
-
                 }
 
             };
--- Work_with_files_2(plus_directiva_na_otpravky_zaprosa_na_spisok_zagryjaemux_failov)/work_with_files.js
+++ Work_with_files_2(plus_directiva_na_otpravky_zaprosa_na_spisok_zagryjaemux_failov)/work_with_files.js
@@ -4,6 +4,17 @@ myApp.controller('mainCtrl', ['$http', '$scope', 'serviceForGetListOfFiles', fun
     var files;
     $scope.ng_model_value_of_text_from_searching;
 
+    $scope.isAjaxLoaderShown = false;
+
+    // Слуханим ивенти
+    $scope.$on('myFilesListChangingEvent', function (event, data) {
+        $scope.isAjaxLoaderShown = data;
+    });
+
+    $scope.$on('myFilesListChangingEvent', function (event, data) {
+       //  alert(data);
+    });
+
     $scope.availableFilesListObj = serviceForGetListOfFiles.serviceForGetListObj;
 
     serviceForGetListOfFiles.getFilesListFromServer();
@@ -17,29 +28,23 @@ myApp.controller('mainCtrl', ['$http', '$scope', 'serviceForGetListOfFiles', fun
     //онклик на загрузку файл на сервер
     $('.upload_files').on('click', function (event) {
 
-        $('.hide').css({
-            'display': 'block'
-        });
+        $scope.$emit('myFilesListChangingEvent', true);
 
         var data = new FormData();
         data.append('file', files[0]);
         console.log(data);
         var promise = $http.post('http://176.36.229.152/ignition/rest/files/upload', data, {
 
-        transformRequest: angular.identity,
+            transformRequest: angular.identity,
             headers: {'Content-Type': undefined}
         });
 
         promise.then(function () {
             serviceForGetListOfFiles.getFilesListFromServer();
-            $('.hide').css({
-                'display': 'none'
-            });
+            $scope.$emit('myFilesListChangingEvent', false);
         }).catch(function (error) {
             console.log(error.status);
-            $('.hide').css({
-            'display': 'none'
-        });
+            $scope.$emit('myFilesListChangingEvent', false);
         });
     });
 }]);
